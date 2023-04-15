43f1a78f0872a51fbd6b34bdc95e91bf2ea8b2ba
jevonwill <jevonwilliamsjr@gmail.com>
2023-04-14 23:17:09 -0400

    Init steps planned, container/book holder, example books

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # practice
+
+1: Loop through array of book objects
+2: Create a book card for each book (i < books.length)
+3: For each card, change innerHTML using placeholder i.e ${books[i][title]}
--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-let library = [];
+let library = [
+    {
+        title: 'Art of War',
+        author: 'Sun Tzu',
+        published: '500BC'
+    },
+    {   title: 'Cask of Amontillado',
+        author: 'Edgar Allen Poe',
+        published: '1837'
+    },
+
+];
+
+const bookHolder = document.querySelector('.bookHolder');
 
